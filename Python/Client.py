6a8f27315b31cb1ceccb0a692f5323f4d440ac69
Jian Shi <jshi.fyu@gmail.com>
2016-11-15 23:04:10 -0600

    Initial python code

---
+++ Python/Client.py
@@ -0,0 +1,26 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('192.168.0.4', 5556))
+
+msgList = ["a", "b", "c", "d"];
+try:
+    for msg in msgList:
+        print(msg)
+
+    #send login message as first one
+    sock.send(bytes("LOGIN" + "\n", "utf-8"))
+    reply = sock.recv(4096)
+    
+    for msg in msgList:    
+        sock.send(bytes('hello' + "\n", "utf-8"))
+        #receive
+        reply = sock.recv(4096)
+        print(reply)
+
+    #send termination message as last one
+    sock.send(bytes("END" + "\n", "utf-8"))
+    reply = sock.recv(4096)
+
+finally:
+    sock.close()
