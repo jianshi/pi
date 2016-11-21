ea6df07f69558820ad91f3c9c93384f33ef5775d
Jian Shi <jshi.fyu@gmail.com>
2016-11-21 05:06:40 +0000

    Update the cloud image cognition call to use local image file, also add image files

--- Python/Python27/cloud_test.py
+++ Python/Python27/cloud_test.py
@@ -5,4 +5,11 @@ from watson_developer_cloud import VisualRecognitionV3 as VisualRecognition
 
 visual_recognition = VisualRecognition('2016-05-20', api_key='c9b2fc34019deb10332121aa27852370b6a9548f')
 
-print(json.dumps(visual_recognition.classify(images_url="https://www.ibm.com/ibm/ginni/images/ginni_bio_780x981_v4_03162016.jpg"), indent=2))
+# example of calling with image url
+#print(json.dumps(visual_recognition.classify(images_url="https://www.ibm.com/ibm/ginni/images/ginni_bio_780x981_v4_03162016.jpg"), indent=2))
+
+# example of calling with image file (zip or jpg/png)
+filename = join(dirname(__file__), '../resources/file.zip')
+with open(filename, 'rb') as imagesToClassify:
+    print(json.dumps(visual_recognition.classify(images_file=imagesToClassify), indent=2))
+
